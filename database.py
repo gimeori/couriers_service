dcc1df68ccb8981b04fddc8219bf196eeb209449
SEA <elizavetka07@gmail.com>
2024-03-12 13:50:39 +0700

    config

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from config import DB_PORT, DB_HOST, DB_NAME,DB_PASS,DB_USER
 
-
-engine=create_engine('postgresql://postgres:gimeori@localhost/couriers_service', echo=True)
+DATABASE_URL='postgresql://%(DB_USER)s:%(DB_PASS)s@%(DB_HOST)s:5432/%(DB_NAME)s'
+engine=create_engine(DATABASE_URL, echo=True)
 Base=declarative_base()
 Session=sessionmaker()
